4458025edde7ffc150dcf604864053c2d1558ec7
petaris@gmail.com <>
2013-05-17 14:33:58 +0000

    Layout of project goals

---
+++ passgen.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+#Written in Python 2.7 syntax
+
+#Passgen is a random password generator
+
+#Features to implement:
+#Feature name - Implemented(X)
+#-Generate a random password
+#-Set a default pw length
+#-Allow user to specify pw length
+#-Set a default number of pw to generate
+#-Allow user to specify number of pw to generate
+#-Allow user to specify upper/lower/both cases
+#-Allow user to specify use of special characters
+#-Allow user to specify no similar characters
+#-Allow user to specify how many times a character is repeated
+#-Allow user to seed with input
+
